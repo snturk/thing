ca9f8ed3b6df54e248b66e4942778d90a96ee598
snturk <muratcansenturk2000@hotmail.com>
2020-01-28 14:10:21 +0300

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div id="mainTitle" v-on:click="clickedTitle()">thing</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import router from '../router/index'
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      clickedTitle(){
+        document.body.style.background = "black";
+        document.getElementById("mainTitle").style.color = "floralwhite";
+        document.getElementById("mainTitle").style.borderColor = "floralwhite";
+        document.getElementById("mainTitle").style.opacity = "0";
+        setTimeout(() => {
+          document.getElementById("mainTitle").style.display = "none";
+          router.push("type");
+          }, 500);
+      }
+    }
+  },
+  created() {
+    document.body.style.background = "#3377ff";
+    document.getElementById("mainTitle").style.color = "black";
+    document.getElementById("mainTitle").style.borderColor = "black";
+    document.getElementById("mainTitle").style.opacity = "1";
+    document.getElementById("mainTitle").style.display = "block";
+  },
+  
 }
 </script>
+
+<style scoped>
+#mainTitle{
+  font-size: 44px;
+  color: black;
+  margin: auto;
+  margin-top: 13%;
+  padding: 15px;
+  border: 2.5px black solid;
+  border-radius: 11px;
+  width: fit-content;
+  transition-duration: 400ms;
+}
+#mainTitle:hover{
+  color: floralwhite;
+  background-color: black;
+  transform: scale(0.87);
+  cursor: pointer;
+}
+</style>
---
+++ src/views/Type.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
