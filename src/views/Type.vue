4ad4f8ac72f366ce33105b1203eabd90bbcf09b3
snturk <muratcansenturk2000@hotmail.com>
2020-02-01 21:08:48 +0300

    added message and changed name of types

--- src/views/Type.vue
+++ src/views/Type.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
+    <div id="typeTitle">choose a type</div>
     <div id="typeContainer"><router-link to="/meditation">
-      <div class="type" v-on:click="() => {}">3 4 2</div>
-      <div class="type" v-on:click="() => {}">2 8 1</div>
-      <div class="type" v-on:click="() => {}">4 7 2</div>,
+      <div class="type" v-on:click="() => {}">4 sec</div>
+      <div class="type" v-on:click="() => {}">7 sec</div>
+      <div class="type" v-on:click="() => {}">11 sec</div>
       </router-link>
     </div>
   </div>
@@ -26,13 +27,19 @@ export default {
 </script>
 
 <style scoped>
+#typeTitle{
+  font-size: 46px;
+  font-family: 'Source Sans Pro', sans-serif;
+  margin-top: 4%;
+}
+
 #typeContainer{
   width: fit-content;
   margin: auto;
-  margin-top: 12%;
+  margin-top: 4%;
 }
 .type{
-  margin-bottom: 110%;
+  margin-bottom: 70%;
   font-family: 'Source Sans Pro', sans-serif;
   font-size: 40px;
   border: 2px floralwhite solid;
