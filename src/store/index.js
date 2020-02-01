b5089aa76229da35aeb8a2f440a33ebf7fde399f
snturk <muratcansenturk2000@hotmail.com>
2020-02-02 01:55:25 +0300

    initialized vuex
    started using vuex to set meditationType variable from every .vue file

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    meditationType: 0
   },
   mutations: {
+    setType: (state) => state.meditationType = 7,
   },
   actions: {
   },
